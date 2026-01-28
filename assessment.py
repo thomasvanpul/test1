6580b90320a32072afcec14293782ab54336f32b
Thomas <tkv25@ic.ac.uk>
2026-01-28 11:36:25 +0000

    Add files via upload

---
+++ assessment.py
@@ -0,0 +1,15 @@
+"""Move around your file system on the command line
+• List files from the command line
+• Clone a GitHub repo using SSH keys as the authentication (command line or with a desktop
+application)
+• Commit and push changes to a git repository (command line or with a desktop application)
+• Run a Python file from the command line
+• Start and stop the Python interactive shell
+• Import Python modules in the interactive shell
+• Run a Python test files and answer questions about the printed output from the tests
+• Fix style issues in Python code identified by the linter"""
+
+
+
+
+
